fa10738e4b3bdc38b232ece8e903ab817155308e
Howard Lee <howardl93@yahoo.com>
2019-08-14 02:43:54 -0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# http://howardlee93.github.io
-
---
+++ precache-manifest.b9d9d4ecabe27840609e5b03847da290.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e405dd446d9420002639b8c5e8b14198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3f0e627c3a5c8de1ee8",
+    "url": "/static/css/main.05737868.chunk.css"
+  },
+  {
+    "revision": "5be94aac9e910a78ed2c",
+    "url": "/static/js/2.a9630097.chunk.js"
+  },
+  {
+    "revision": "f3f0e627c3a5c8de1ee8",
+    "url": "/static/js/main.0d1f3494.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "82dcdf3b305b9d4e06a4416fa6d0d11a",
+    "url": "/static/media/1.82dcdf3b.png"
+  },
+  {
+    "revision": "abd82bd8e219a9b748573e59659d7094",
+    "url": "/static/media/2.abd82bd8.png"
+  },
+  {
+    "revision": "c94aad79f98060588dae89767763ab31",
+    "url": "/static/media/asteroid.c94aad79.png"
+  },
+  {
+    "revision": "18fdc4e4f3844867b506954a73c92741",
+    "url": "/static/media/css.18fdc4e4.png"
+  },
+  {
+    "revision": "a2ff6c783892a9e8675de0c4070f12e6",
+    "url": "/static/media/java.a2ff6c78.png"
+  },
+  {
+    "revision": "7a79b7ded7e134ed7092be2f44370807",
+    "url": "/static/media/logo.7a79b7de.svg"
+  },
+  {
+    "revision": "50ae88ab5a56b84e9f4e21609b61a588",
+    "url": "/static/media/magnifiy.50ae88ab.png"
+  },
+  {
+    "revision": "12a66465f5d591a3730326e6f96d2344",
+    "url": "/static/media/prof.12a66465.png"
+  },
+  {
+    "revision": "5f641a0d43b7ed35d20b0da464da3134",
+    "url": "/static/media/redux.5f641a0d.png"
+  },
+  {
+    "revision": "5c6df966a67188c0296804d8253181a3",
+    "url": "/static/media/resume.5c6df966.pdf"
+  },
+  {
+    "revision": "28ee737079cdd7bd63088508652d9756",
+    "url": "/static/media/svg.28ee7370.png"
+  },
+  {
+    "revision": "9d1438047c4512778cd85156a96c9fa2",
+    "url": "/static/media/swift.9d143804.svg"
+  },
+  {
+    "revision": "86b2d552ebeeccaf5024d2e7b1df3a44",
+    "url": "/static/media/weather.86b2d552.png"
+  }
+]);
